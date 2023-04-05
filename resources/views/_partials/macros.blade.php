e6ad42ec4ceeeae8945c0908da5e02293b349aec
emre-unaldi <emree.unaldi@gmail.com>
2023-04-05 16:47:32 +0300

    Laravel environment created and pages designed with Bootstrap@5

---
+++ resources/views/_partials/macros.blade.php
@@ -0,0 +1,5 @@
+<img 
+  width="200px"
+  src="{{asset('assets/img/logo/kctek.png')}}" 
+  alt="Kctek" 
+>
---
+++ resources/views/content/home/home.blade.php
@@ -0,0 +1,130 @@
+@extends('layouts/contentNavbarLayout')
+
+@section('title', 'Dashboard - Analytics')
+
+@section('vendor-style')
+<link rel="stylesheet" href="{{asset('assets/vendor/libs/apex-charts/apex-charts.css')}}">
+@endsection
+
+@section('vendor-script')
+<script src="{{asset('assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+@endsection
+
+@section('page-script')
+<script src="{{asset('assets/js/dashboards-analytics.js')}}"></script>
+@endsection
+
+@section('content')
+<div class="row">
+  <div class="col-lg-12 mb-4 order-0">
+    <div class="card">
+      <div class="d-flex align-items-end row">
+        <div class="col-sm-7">
+          <div class="card-body">
+            <h5 class="card-title text-primary">Welcome Emre 🎉</h5>
+            <p class="mb-4"><span class="fw-bold">Kctek Database Backup Automation</span></p>
+          </div>
+        </div>
+        <div class="col-sm-5 text-center text-sm-left">
+          <div class="card-body pb-0 px-0 px-md-4">
+            <img src="{{asset('assets/img/illustrations/man-with-laptop-light.png')}}" height="140" alt="View Badge User" data-app-dark-img="illustrations/man-with-laptop-dark.png" data-app-light-img="illustrations/man-with-laptop-light.png">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+
+  <!-- Order Statistics -->
+  <div class="col-md-12 col-lg-12 col-xl-12 order-0 mb-4">
+    <div class="card h-100">
+      <div class="card-header d-flex align-items-center justify-content-between pb-0">
+        <div class="card-title mb-0">
+          <h5 class="m-0 me-2">Order Statistics</h5>
+          <small class="text-muted">42.82k Total Sales</small>
+        </div>
+        <div class="dropdown">
+          <button class="btn p-0" type="button" id="orederStatistics" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <i class="bx bx-dots-vertical-rounded"></i>
+          </button>
+          <div class="dropdown-menu dropdown-menu-end" aria-labelledby="orederStatistics">
+            <a class="dropdown-item" href="javascript:void(0);">Select All</a>
+            <a class="dropdown-item" href="javascript:void(0);">Refresh</a>
+            <a class="dropdown-item" href="javascript:void(0);">Share</a>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-center mb-3">
+          <div class="d-flex flex-column align-items-center gap-1">
+            <h2 class="mb-2">8,258</h2>
+            <span>Total Orders</span>
+          </div>
+          <div id="orderStatisticsChart"></div>
+        </div>
+        <ul class="p-0 m-0">
+          <li class="d-flex mb-4 pb-1">
+            <div class="avatar flex-shrink-0 me-3">
+              <span class="avatar-initial rounded bg-label-primary"><i class='bx bx-mobile-alt'></i></span>
+            </div>
+            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+              <div class="me-2">
+                <h6 class="mb-0">Electronic</h6>
+                <small class="text-muted">Mobile, Earbuds, TV</small>
+              </div>
+              <div class="user-progress">
+                <small class="fw-semibold">82.5k</small>
+              </div>
+            </div>
+          </li>
+          <li class="d-flex mb-4 pb-1">
+            <div class="avatar flex-shrink-0 me-3">
+              <span class="avatar-initial rounded bg-label-success"><i class='bx bx-closet'></i></span>
+            </div>
+            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+              <div class="me-2">
+                <h6 class="mb-0">Fashion</h6>
+                <small class="text-muted">T-shirt, Jeans, Shoes</small>
+              </div>
+              <div class="user-progress">
+                <small class="fw-semibold">23.8k</small>
+              </div>
+            </div>
+          </li>
+          <li class="d-flex mb-4 pb-1">
+            <div class="avatar flex-shrink-0 me-3">
+              <span class="avatar-initial rounded bg-label-info"><i class='bx bx-home-alt'></i></span>
+            </div>
+            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+              <div class="me-2">
+                <h6 class="mb-0">Decor</h6>
+                <small class="text-muted">Fine Art, Dining</small>
+              </div>
+              <div class="user-progress">
+                <small class="fw-semibold">849k</small>
+              </div>
+            </div>
+          </li>
+          <li class="d-flex">
+            <div class="avatar flex-shrink-0 me-3">
+              <span class="avatar-initial rounded bg-label-secondary"><i class='bx bx-football'></i></span>
+            </div>
+            <div class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+              <div class="me-2">
+                <h6 class="mb-0">Sports</h6>
+                <small class="text-muted">Football, Cricket Kit</small>
+              </div>
+              <div class="user-progress">
+                <small class="fw-semibold">99</small>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <!--/ Order Statistics -->
+
+</div>
+@endsection
