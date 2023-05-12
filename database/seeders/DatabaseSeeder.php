4f078f8bb80495cb20b8f4310e250300d5725550
emre-unaldi <emree.unaldi@gmail.com>
2023-05-12 10:16:24 +0300

    Finished Projects

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //\App\Models\User::factory(30)->create();
+        $this->call(UserSeeder::class);
     }
 }
